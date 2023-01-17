2eff6ebf40b08da37935d9cfca1e0e3d6688bb66
Zhang.H.N <zhang.h.n@foxmail.com>
2023-01-17 23:00:35 +0800

    use output path as param

--- annopro/data_procession/__init__.py
+++ annopro/data_procession/__init__.py
@@ -1,13 +1,11 @@
 from annopro.data_procession.data_predict import Data_process
-import os
 
 
-def process(protein_fasta_file: str, profeat_file: str, output_dir: str):
-    if protein_fasta_file == None:
+def process(proteins_fasta_file: str, profeat_file: str, save_file: str):
+    if proteins_fasta_file == None:
         raise ValueError("Must provide the input fasta sequences.")
 
-    save_file = os.path.join(output_dir, "protein.pkl")
     data = Data_process(protein_file=profeat_file,
-                        split_file=protein_fasta_file, 
+                        proteins_fasta_file=proteins_fasta_file, 
                         save_file=save_file, num=1484)
     data.calculate_feature(row_num=39, size=(39, 39, 7))
