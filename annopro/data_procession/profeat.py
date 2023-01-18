ddf9b0e3c6e8f90a74dd9977f97555a7aa2fd737
Zhang.H.N <zhang.h.n@foxmail.com>
2023-01-19 00:31:29 +0800

    Merge to profeat module

--- annopro/data_procession/profeat.py
+++ annopro/data_procession/profeat.py
@@ -1,10 +1,13 @@
 import annopro.data_procession._libprofeat as libprofeat
 import annopro.data_procession._libprofeatconfig as libprofeatconfig
 import os
+from collections import defaultdict
+import pandas as pd
+
 
 def run(protein_fasta_file: str, output_dir: str):
     """
-    run
+    run profeat
     
     """
     if not output_dir.endswith(os.sep):
@@ -20,3 +23,32 @@ def run(protein_fasta_file: str, output_dir: str):
         output_dir_len,
         config_dir,
         config_dir_len)
+
+
+def profeat_to_df(data_path: str) -> pd.DataFrame:
+    """
+    Read profeat features as `pd.DataFrame`.
+    """
+    with open(data_path, "r") as file:
+        protein_list = list()
+        feature_list = defaultdict(list)
+        protein_line_index = -1
+        line: str
+        for rol_index, line in enumerate(file, 1):
+            line = line.strip()
+            if protein_line_index == rol_index -1:
+                continue
+
+            if line.startswith(">"):
+                protein_line_index = rol_index
+                protein = line[1:]
+                protein_list.append(protein)
+            else:
+                protein = protein_list[-1]
+                for feature in line.split():
+                    try:
+                        feature_list[protein].append(float(feature))
+                    except ValueError:
+                        print(f"Invalid feature {feature} for {protein} at line {rol_index}")
+    
+    return pd.DataFrame(feature_list).T
