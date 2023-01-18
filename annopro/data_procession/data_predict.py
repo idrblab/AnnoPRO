16c2c7988f5cfced68933099e4d5e84446157bbd
Zhang.H.N <zhang.h.n@foxmail.com>
2023-01-19 00:32:09 +0800

    update import

--- annopro/data_procession/data_predict.py
+++ annopro/data_procession/data_predict.py
@@ -1,17 +1,13 @@
-from annopro.data_procession.consine_distances import load_data, MinMaxScaleClip
+import numpy as np
 import pandas as pd
 import pickle
 
-import numpy as np
 from importlib import resources
 from sklearn.metrics.pairwise import cosine_similarity
-from annopro.data_procession.utils import Ontology
-from annopro.data_procession.error import profeat_to_df
+from annopro.data_procession.utils import Ontology, load_data, MinMaxScaleClip
+from annopro.data_procession.profeat import profeat_to_df
+from annopro import data
 from fasta import FASTA
-
-
-DATA_PACKAGE = "annopro.data"
-
 
 class Data_process():
 
@@ -44,23 +40,25 @@ class Data_process():
         proteins_f = profeat_to_df(self.protein_file)
         proteins_f.dropna(axis=0, inplace=True)
         feature_data = proteins_f.iloc[:, :self.num]
-        with resources.open_text(DATA_PACKAGE, "cafa4_del.csv") as cafa4_del:
+        with resources.open_text(data, "cafa4_del.csv") as cafa4_del:
             mia_data = load_data(cafa4_del, 1485)
         mia_data.columns = range(len(mia_data.columns))
         feature_data = (feature_data - mia_data.min()) / \
             ((mia_data.max() - mia_data.min()) + 1e-8)
-        self.feature_data = feature_data
+        self.feature_data:pd.DataFrame = feature_data
         self.proteins = list(proteins_f.index)
-        with resources.open_binary(DATA_PACKAGE, self.grid_file) as gf:
+        with resources.open_binary(data, self.grid_file) as gf:
             self.data_grid = pickle.load(gf)
-        with resources.open_binary(DATA_PACKAGE, self.assess_file) as af:
+        with resources.open_binary(data, self.assess_file) as af:
             self.row_asses = pickle.load(af)
-        with resources.open_text(DATA_PACKAGE, self.prosim_file) as pf:
+        with resources.open_text(data, self.prosim_file) as pf:
             prosim_data = load_data(pf, 1485)
+        # this will be minmax by column
         prosim_standard = MinMaxScaleClip(prosim_data)
-        prosim_map = np.mat(prosim_standard)
-        prosim_map = MinMaxScaleClip(prosim_map)
-        self.prosim_map = np.mat(prosim_map)
+        prosim_map = prosim_standard.to_numpy()
+        # this will be global minmax scale
+        prosim_map:np.ndarray = MinMaxScaleClip(prosim_map)
+        self.prosim_map = prosim_map
         self.go = Ontology()
 
     def calculate_feature(self, row_num, size):
@@ -74,15 +72,9 @@ class Data_process():
         data_grid = self.data_grid
         row_asses = self.row_asses
         proteins = self.proteins
-        feature_data = self.feature_data
-        # 构建蛋白蛋白相似性
-        result = np.mat(feature_data)
-        consine_similarity = np.array(
-            1)-cosine_similarity(result, self.prosim_map)
-        # row, col = np.diag_indices_from(consine_similarity)
-        # consine_similarity[row,col]=0
-        print(proteins)
-        print(protein_seqs)
+        result = self.feature_data.to_numpy()
+        consine_similarity = 1-cosine_similarity(result, self.prosim_map)
+ 
         for i, protein in enumerate(proteins):
             if protein in protein_seqs:
                 col_list = np.zeros(size)
@@ -98,8 +90,7 @@ class Data_process():
                     feature_index = row_asses[j]
                     row = j % row_num
                     col = j//row_num
-                    col_list[col][row][index] = feature_data.iloc[i,
-                                                                  feature_index]
+                    col_list[col][row][index] = self.feature_data.iloc[i, feature_index]
                 feature_all.append(col_list)
         data_t = [protein_all, sequences_all, feature_all, prosim_all]
         data_t = pd.DataFrame(data_t)
