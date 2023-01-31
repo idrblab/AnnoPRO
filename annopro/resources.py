d8a993af0d7eaf19f64c72ace5bff6db840a417d
Zhang.H.N <zhang.h.n@foxmail.com>
2023-01-31 22:26:53 +0800

    update resource url

--- annopro/resources.py
+++ annopro/resources.py
@@ -11,55 +11,55 @@ os.makedirs(RESOURCE_DIR, exist_ok=True)
 
 RESOURCE_DICT = {
     "cafa_train.pkl": {
-        "url": "http://localhost:8000/annopro/data/cafa_train.pkl",
+        "url": "https://promap.oss-cn-hangzhou.aliyuncs.com/data/cafa_train.pkl",
         "md5sum": "07d3e4334c31c914efec3f52cae5e498"
     },
     "cafa4_del.csv": {
-        "url": "http://localhost:8000/annopro/data/cafa4_del.csv",
+        "url": "https://promap.oss-cn-hangzhou.aliyuncs.com/data/cafa4_del.csv",
         "md5sum": "d00b71439084cb19b7d3d0d4fbbaa819"
     },
     "cafa4.dmnd": {
-        "url": "http://localhost:8000/annopro/data/cafa4.dmnd",
+        "url": "https://promap.oss-cn-hangzhou.aliyuncs.com/data/cafa4.dmnd",
         "md5sum": "a2a6cba9af26dbe1911e14a306db2712"
     },
     "data_grid.pkl": {
-        "url": "http://localhost:8000/annopro/data/data_grid.pkl",
+        "url": "https://promap.oss-cn-hangzhou.aliyuncs.com/data/data_grid.pkl",
         "md5sum": "fb2d2d86a4bc21c6e60fac996b3a90d3"
     },
     "go.pkl": {
-        "url": "http://localhost:8000/annopro/data/go.pkl",
+        "url": "https://promap.oss-cn-hangzhou.aliyuncs.com/data/go.pkl",
         "md5sum": "8d7a975d38a4af670b0370f4ea722a2a"
     },
     "go.txt": {
-        "url": "http://localhost:8000/annopro/data/go.txt",
+        "url": "https://promap.oss-cn-hangzhou.aliyuncs.com/data/go.txt",
         "md5sum": "1dae308468fa00ae6d5796fd22c65044"
     },
     "row_asses.pkl": {
-        "url": "http://localhost:8000/annopro/data/row_asses.pkl",
+        "url": "https://promap.oss-cn-hangzhou.aliyuncs.com/data/row_asses.pkl",
         "md5sum": "bf9bb1eda744a60c381d19b275ac6f33"
     },
     "terms_biological_process.pkl": {
-        "url": "http://localhost:8000/annopro/data/terms_biological_process.pkl",
+        "url": "https://promap.oss-cn-hangzhou.aliyuncs.com/data/terms_biological_process.pkl",
         "md5sum": "e79cf5e006432c19606b8a482cf7ddfa"
     },
     "terms_cellular_component.pkl": {
-        "url": "http://localhost:8000/annopro/data/terms_cellular_component.pkl",
+        "url": "https://promap.oss-cn-hangzhou.aliyuncs.com/data/terms_cellular_component.pkl",
         "md5sum": "8cf58075eba65e2bb710566e4ef93f42"
     },
     "terms_molecular_function.pkl": {
-        "url": "http://localhost:8000/annopro/data/terms_molecular_function.pkl",
+        "url": "https://promap.oss-cn-hangzhou.aliyuncs.com/data/terms_molecular_function.pkl",
         "md5sum": "002c3696fbca0061402a68129b35dcd4"
     },
     "bp.h5": {
-        "url": "http://localhost:8000/annopro/model_param/bp.h5",
+        "url": "https://promap.oss-cn-hangzhou.aliyuncs.com/model_param/bp.h5",
         "md5sum": "7e19158e5252a70ff831f5f583b1c2ed"
     },
     "cc.h5": {
-        "url": "http://localhost:8000/annopro/model_param/cc.h5",
+        "url": "https://promap.oss-cn-hangzhou.aliyuncs.com/model_param/cc.h5",
         "md5sum": "73876beec9370ff56b58878cf4446d2c"
     },
     "mf.h5": {
-        "url": "http://localhost:8000/annopro/model_param/mf.h5",
+        "url": "https://promap.oss-cn-hangzhou.aliyuncs.com/model_param/mf.h5",
         "md5sum": "f4fb632f553afeb45571a29e46286bb8"
     }
 }
